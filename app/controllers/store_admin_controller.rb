347ccc932040d5c781dc1fbaee6fa155a0b1ce25
Jesse Wenger <wengerj@gmail.com>
2021-05-04 22:02:02 +0000

    Done.

---
+++ app/controllers/store_admin_controller.rb
@@ -0,0 +1,14 @@
+class StoreAdminController < ApplicationController
+  layout "admin"
+
+  def invoice
+    render :layout => false
+  end
+
+  def orders
+    render :layout => "order_administration"
+  end
+
+  def home
+  end
+end
